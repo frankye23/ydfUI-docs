7b9caaee699d631dbab7d5b63ba595957030aacd
frankye23 <dafeng.ye@intchains.com>
2023-01-17 18:10:28 +0800

    feat: init project

---
+++ docs/.vuepress/components/Input.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex">
+    <div class="w-full inline-flex items-center justify-end">
+      <input
+        class="
+          form-control
+          block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          h-14
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+        "
+          :placeholder="placeholder"
+          :value="modelValue" @input="updateValue"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { useRouter } from 'vue-router';
+
+  const props = defineProps(['placeholder','modelValue'])
+  const emit = defineEmits(['update:modelValue'])
+
+  const updateValue = (event: any) => {
+    emit('update:modelValue', event.target.value)
+  }
+
+  const router = useRouter()
+  const goBulkSearch = () => {
+    router.push('/search')
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ docs/guide/index.md
@@ -0,0 +1,12 @@
+# 基于Tailwind封装组件
+
+## Input 输入框
+
+<Input class="mt-4" />
+
+## Select 下拉选择
+<Select class="flex text-left" />
+
+<script lang="ts" setup>
+  const options = []
+</script>
